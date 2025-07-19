e55142d660e326d304986f5141e87ca6e83f391b
michael.milich <michael.milich1@gmail.com>
2025-07-19 11:44:15 +0300

    minor changes. next step: implement fen_board_to_fen_string. second step - implement Move structure logic. third step - impleent a function For FEN_BOARD that gets a move and generates the new FEN Board

--- include/fen_utils.h
+++ include/fen_utils.h
@@ -7,14 +7,6 @@
 #include <stdio.h>
 
 typedef struct {
-    char time_control[20];  // Fixed size for time format (e.g., "10", "10+3", "600+0")
-    int move_number;
-    char fen[100];         // Fixed size for FEN string (standard FEN is ~90 chars max)
-    int elo;
-    Move move;
-} FEN_Plus;
-
-typedef struct {
     char board[8][8];
     int move_number;
     int en_passant_square_file; // a-h
@@ -25,6 +17,13 @@ typedef struct {
     int fullmove_number;
 } FEN_Board;
 
+typedef struct {
+    char time_control[20];  // Fixed size for time format (e.g., "10", "10+3", "600+0")
+    FEN_Board board;
+    int elo;
+    Move move;
+} FEN_Plus;
+
 
 FEN_Plus *starting_position_fen_plus(char *time_control, int elo, Move *uci_move);
 void next_fen_plus(FEN_Plus *fen_plus, FEN_Plus *next_fen_plus);
@@ -32,5 +31,6 @@ FEN_Plus **generate_list_of_fen_plus(FEN_Plus *starting_fen_plus, char *pgn_stri
 void free_fen_plus(FEN_Plus *fen_plus);
 
 bool create_fen_board(FEN_Board *output, char *fen_string);
+bool fen_board_to_fen_string(FEN_Board *board, char *fen_string_out); // TODO: Implement this
 
 #endif
